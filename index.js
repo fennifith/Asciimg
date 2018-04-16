3a54f8caa42da937ebdc527c18ce0f68426ac16e
James Fenn <18jafenn90@gmail.com>
2018-04-16 18:25:54 -0400

    COLOR ACCURACY somewhat

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ program.version('1.0.0')
 					var string = "";
 					for (var char = 0; char < width; char++) {
 						var startIndex = Math.floor((charRow * heightMult * pixels.shape[0]) + (char * widthMult)) * pixels.shape[2];
-						var red = Math.floor(pixels.data[startIndex] * (16/255)) * (255/16);
-						var green = Math.floor(pixels.data[startIndex + 1] * (16/255)) * (255/16);
-						var blue = Math.floor(pixels.data[startIndex + 2] * (16/255)) * (255/16);
+						var red = pixels.data[startIndex];
+						var green = pixels.data[startIndex + 1];
+						var blue = pixels.data[startIndex + 2];
 						var merges = 0;
 						
 						for (var pixRow = Math.floor(charRow * heightMult); pixRow < ((charRow + 1) * heightMult) - 1; pixRow++) {
@@ -57,63 +57,86 @@ program.version('1.0.0')
 								merges++;
 								
 								var index = ((pixRow * pixels.shape[0]) + pixel) * pixels.shape[2];
-								red = Math.floor(((red * merges) + (Math.floor(pixels.data[index] * (16/255)) * (255/16))) / (merges + 1));
-								green = Math.floor(((green * merges) + (Math.floor(pixels.data[index + 1] * (16/255)) * (255 / 16))) / (merges + 1));
-								blue = Math.floor(((blue * merges) + (Math.floor(pixels.data[index + 2] * (16/255)) * (255 / 16))) / (merges + 1));
+								red = Math.floor(((red * merges) + pixels.data[index]) / (merges + 1));
+								green = Math.floor(((green * merges) + pixels.data[index + 1]) / (merges + 1));
+								blue = Math.floor(((blue * merges) + pixels.data[index + 2]) / (merges + 1));
 							}
 						}
 
-						var colorId = red + "," + green + "," + blue;		
+						red = Math.floor(red);
+						green = Math.floor(green);
+						blue = Math.floor(blue);
+						var redId = Math.floor(red * 16 / 255) * 255 / 16;
+						var greenId = Math.floor(green * 16 / 255) * 255 / 16;
+						var blueId = Math.floor(blue * 16 / 255) * 255 / 16;
+
+						var colorId = redId + "," + greenId + "," + blueId;		
 						if (!colors[colorId]) {
 							colors[colorId] = String.fromCharCode(65 + Math.floor(Math.random() * 26));
 						}
 
 						var color = chalk.rgb(red, green, blue).bgRgb(Math.max(0, red - 20), Math.max(0, green - 20), Math.max(0, blue - 20));
 						if (!program.usergb) {
-							if (red == green && green == blue) {
-								if (red > 125) {
+							if (redId == greenId && greenId == blueId) {
+								if (red > 190) {
 									color = chalk.gray.bgWhite;
-								} else {
+								} else if (red > 125) {
+									color = chalk.black.bgWhite;
+								} else if (red > 64) {
 									color = chalk.white.bgBlack;
+								} else {
+									color = chalk.gray.bgBlack;
 								}
-							} else if (red > green && red > blue) {
-								if (green > blue) {
+							} else if (redId > greenId && redId > blueId) {
+								if (red > 200) {
+									color = chalk.white.bgRed;
+								} else if (red < 50) {
+									color = chalk.black.bgRed;
+								} else if (greenId > blueId) {
 									color = chalk.green.bgRed;
-								} else if (blue > green) {
+								} else if (blueId > greenId) {
 									color = chalk.blue.bgRed;
 								} else {
 									color = chalk.cyan.bgRed;
 								}
-							} else if (green > red && green > blue) {
-								if (red > blue) {
+							} else if (greenId > redId && greenId > blueId) {
+								if (green > 200) {
+									color = chalk.white.bgGreen;		
+								} else if (green < 50) {
+									color = chalk.black.bgGreen;
+								} else if (redId > blueId) {
 									color = chalk.red.bgGreen;
-								} else if (blue > red) {
+								} else if (blueId > redId) {
 									color = chalk.blue.bgGreen;
 								} else {
 									color = chalk.magenta.bgGreen;
 								}
-							} else if (blue > red && blue > green) {
-								if (red > green) {
+							} else if (blueId > redId && blueId > greenId) {
+								if (blue > 200) {
+									color = chalk.white.bgBlue;
+								} else if (blue < 50) {
+									color = chalk.black.bgBlue;
+								} else if (redId > greenId) {
 									color = chalk.red.bgBlue;
-								} else if (green > red) {
+								} else if (greenId > redId) {
 									color = chalk.green.bgBlue;
 								} else {
 									color = chalk.yellow.bgBlue;
 								}
-							} else if (red == green) {
-								if (blue > green) {
+							} else if (redId == greenId) {
+								if (blueId > greenId) {
 									color = chalk.yellow.bgBlue;
 								} else {
 									color = chalk.yellow.bgYellow;
 								}
-							} else if (green == blue) {
-								if (red > green) {
+							} else if (greenId == blueId) {
+								if (redId > greenId) {
 									color = chalk.cyan.bgRed;
 								} else {
 									color = chalk.cyan.bgCyan;
 								}
-							} else if (red == blue) {
-								if (green > red) {
+							} else if (redId == blueId) {
+								if (greenId > redId) {
 									color = chalk.magenta.bgGreen;
 								} else {
 									color = chalk.magenta.bgMagenta;
